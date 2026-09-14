bf0c2abbdea1cf86562fbd8b0b62c16a230d6bea
ma9938 <ma9938@g.rit.edu>
2026-09-14 11:07:02 +0400

    my first git

---
+++ hbday.py
@@ -0,0 +1,17 @@
+months_in_year = 12
+
+def happy_birthday():
+    name = input("Enter your name:")
+    month = input("Enter your Birth month:")
+    day = input("Enter your Birth date:")
+    year = input("Enter your Birth year:")
+    print(name, ", your birthday is on", day, month, year)
+
+    
+def main():
+ happy_birthday()
+
+
+main()
+
+
